ef25757123bc672a507d2dfe39ad7d0e2159dd8f
WinG4merBR <wingamerbr@foxybot.win>
2024-03-25 23:20:42 -0300

    Create a webserver to provide files to all CakeyFox's websites

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3001, () => {
+    console.info("[LOTUS] - Server running at port 3001");
+});
