c4f1e59e59dbd04b8433312a6b66f4e430f0f32d
WinG4merBR <wingamerbr@foxybot.win>
2024-05-05 17:15:47 -0300

    Update CSS

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+    res.send();
+});
+
 app.listen(3001, () => {
     console.info("[LOTUS] - Server running at port 3001");
 });
