92ded04b922a8f2786fdc4e4a7cee5addbecff14
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2024-09-01 12:53:55 -0300

    Update ServerInstance.kt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.cli.jvm.main
+
 plugins {
     kotlin("jvm") version "2.0.0"
     id("io.ktor.plugin") version "2.3.11"
@@ -18,6 +20,10 @@ dependencies {
     implementation("io.ktor:ktor-server-netty:2.3.12")
     testImplementation("io.ktor:ktor-server-test-host:2.3.12")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+application {
+    mainClass.set("live.cakeyfox.LauncherKt")
 }
 
 tasks.test {
--- src/main/kotlin/live/cakeyfox/lotus/server/ServerInstance.kt
+++ src/main/kotlin/live/cakeyfox/lotus/server/ServerInstance.kt
@@ -6,17 +6,24 @@ import io.ktor.server.http.content.*
 import io.ktor.server.netty.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import java.net.BindException
 
 class ServerInstance {
     fun start() {
-        embeddedServer(Netty, port = 3001) {
-            routing {
-                staticResources("/", "static")
-                print("Server started on port 3001")
-                get("/") {
-                    call.respondText { "owo" }
+        try {
+            embeddedServer(Netty, port = 3001) {
+                routing {
+                    staticResources("/", "static")
+                    print("Server started on port 3001")
+                    get("/") {
+                        call.respondText { "owo" }
+                    }
                 }
-            }
-        }.start(wait = true)
+            }.start(wait = true)
+        } catch (e: BindException) {
+            println("Port 3001 is already in use!")
+        } catch (e: Exception) {
+            println("An error occurred: ${e.message}")
+        }
     }
 }
